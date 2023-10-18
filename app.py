7ce73499b7f7a6365cb0771c5ce44ffaa85b222d
younes <yoelkh@fp.insjoaquimmir.cat>
2023-10-18 19:57:02 +0200

    Agregada integracion con SQLite

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('database.db')
+    return db
 
-@app.route('/init')
-def init():
-    return render_template('index.html')
+@app.before_request
+def before_request():
+    g.db = get_db()
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def home():
+    return render_template('hello.html')
+
+@app.route('/products/list')
+def list_products():
+    cursor = g.db.cursor()
+    cursor.execute('SELECT * FROM products')
+    productos = cursor.fetchall()
+    return render_template('/products/list.html', products=productos)
 
 if __name__ == '__main__':
     app.run()
